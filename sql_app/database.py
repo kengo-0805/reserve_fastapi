b84c76f78d4e7a3c2a3a1e99d27f6f77b4e8e0d6
kengoMBP <kengo0805ooo@gmail.com>
2025-08-18 23:42:11 +0900

    add:データベースのモデルを定義

---
+++ sql_app/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# データベースの格納先
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+# 操作を行う基盤
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
+)
+# データベースの一連の流れ
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, )
+Base = declarative_base()
